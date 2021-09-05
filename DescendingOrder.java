cf2a69600ff288887b0c1bdb7f617f3435104a55
arimekishou <fgfdgdfj@mail.ru>
2021-09-05 14:45:14 +0300

    files

---
+++ DescendingOrder.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.valueOf(String.join("", array));
+    }
+}
